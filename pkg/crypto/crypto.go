672025d6b49af5293a6c0763362fc083c7f8d258
ngygbr <ngabesz20@icloud.com>
2022-04-04 15:50:45 +0200

    improve crypto

--- pkg/crypto/crypto.go
+++ pkg/crypto/crypto.go
@@ -3,8 +3,53 @@ package crypto
 import (
 	"crypto/aes"
 	"crypto/cipher"
+	"crypto/rand"
 	"encoding/hex"
+	"encoding/json"
+	"io"
+	"log"
+	"mpp/pkg/model"
 )
+
+func generateKey() (string, error) {
+	bytes := make([]byte, 32)
+	if _, err := rand.Read(bytes); err != nil {
+		return "", err
+	}
+
+	key := hex.EncodeToString(bytes)
+
+	return key, nil
+}
+
+func EncryptCard(cardToEncrypt model.CreditCard, keyString string) (string, string, error) {
+
+	marshalledCard, err := json.Marshal(cardToEncrypt)
+	if err != nil {
+		log.Fatalf("Error occured during marshaling. Error: %s", err.Error())
+	}
+
+	key, _ := hex.DecodeString(keyString)
+	plaintext := marshalledCard
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", "", err
+	}
+
+	aesGCM, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", "", err
+	}
+
+	nonce := make([]byte, aesGCM.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return "", "", err
+	}
+
+	ciphertext := aesGCM.Seal(nonce, nonce, plaintext, nil)
+	return string(ciphertext), keyString, nil
+}
 
 func DecryptCard(encryptedString string, keyString string) (string, error) {
 
