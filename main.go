dff9c6a7abe87980b4337148360ae455289245f3
Nagy Gábor <nagygabor@Nagy-MacBook-Pro.local>
2022-05-09 19:31:56 +0200

    l

--- main.go
+++ main.go
@@ -13,13 +13,13 @@ func main() {
 	config := utils.GetConfig()
 
 	if _, err := db.Connect(config.BadgerTmp); err != nil {
-		panic("failed to connect database")
+		panic(err)
 	}
 
 	go controller.SetSettledAfter24Hours()
 
 	if err := api.Init(); err != nil {
-		panic("failed init")
+		panic(err)
 	}
 
 }
